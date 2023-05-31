2e523ff9f246494182e254783645d139a8183536
Gaweł Banasik <83306952+Gawello@users.noreply.github.com>
2023-05-31 20:48:24 +0200

    Canvas + Rectangle

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-class Canvas:
+import numpy as np
+from PIL import Image
 
-    def __init__(self, a, b, color):
-        self.a = a
-        self.b = b
+
+class Canvas:
+    """
+    Object where all shapes are going to be drawn
+    """
+
+    def __init__(self, height, width, color):
+        self.height = height
+        self.width = width
         self.color = color
 
-    def make(self):
-        pass
+        # Create a 3D numpy array of zeros
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        # Change [0,0,0] with user given values for color
+        self.data[:] = self.color
+
+    def make(self, image_path):
+        """Converts the current array into an image file"""
+        img = Image.fromarray(self.data, 'RGB')
+        img.save(image_path)
 
 
 class Rectangle:
-
-    def __init__(self, x, y, a, b, color):
+    """A rectangle shape that can be drawn on a Canvas object"""
+    def __init__(self, x, y, height, width, color):
         self.x = x
         self.y = y
-        self.a = a
-        self.b = b
+        self.height = height
+        self.width = width
         self.color = color
 
     def draw(self, canvas):
-        pass
+        """Draws itself into the Canvas"""
+        # Changes a slice of the array with new values
+        canvas.data[self.x: self.x + self.width, self.y: self.y + self.height] = self.color
 
 
 class Square:
